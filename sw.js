f88590630985adb53f5afa9723154f5619c91c22
andre <mininao4@hotmail.com>
2026-05-24 16:41:20 -0300

    corrigido cache pwa

--- sw.js
+++ sw.js
@@ -1,35 +1,25 @@
-const CACHE_NAME = "desafio-semanal-v1"
+const CACHE_NAME = "desafio-semanal-v2"
 
 const arquivos = [
-
   "./",
   "./index.html",
   "./style.css",
   "./script.js",
-  "./manifest.json"
-
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ]
 
 self.addEventListener("install", event => {
-
   event.waitUntil(
-
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(arquivos))
-
+    caches.open(CACHE_NAME).then(cache => cache.addAll(arquivos))
   )
 })
 
 self.addEventListener("fetch", event => {
-
   event.respondWith(
-
-    caches.match(event.request)
-      .then(response => {
-
-        return response || fetch(event.request)
-
-      })
-
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request)
+    })
   )
 })
